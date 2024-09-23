5445c3ce392e3afc255f28e56240b7fdc6bbb8d1
juger1 <gajehaj8@gmail.com>
2024-09-23 19:52:12 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -38,16 +38,8 @@ class Database:
 
 
 db = Database(DATABASE_URL, DATABASE_NAME)
+
 """
-   _____                    __  __         _    _              _       _______           _     
-  / ____|                  |  \/  |       | |  | |            ( )     |__   __|         | |    
- | |  __  _ __  ___  _   _ | \  / |  __ _ | |_ | |_  ___  _ __|/ ___     | |  ___   ___ | |__  
- | | |_ || '__|/ _ \| | | || |\/| | / _` || __|| __|/ _ \| '__| / __|    | | / _ \ / __|| '_ \ 
- | |__| || |  |  __/| |_| || |  | || (_| || |_ | |_|  __/| |    \__ \    | ||  __/| (__ | | | |
-  \_____||_|   \___| \__, ||_|  |_| \__,_| \__| \__|\___||_|    |___/    |_| \___| \___||_| |_|
-                      __/ |                                                                    
-                     |___/                                                                     
-Author: GreyMatter's Tech
-GitHub: https://GreyMattersTech.com/GitHub
-Website: https://GreyMattersTech.com
+Author: StupidBoi
+Telegram: https://t.me/StupidBoi69
 """
