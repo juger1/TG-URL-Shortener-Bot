2b65b715de35eb904ab01fcc818f03a5540b9be0
juger1 <gajehaj8@gmail.com>
2024-09-23 19:49:47 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -1,36 +1,23 @@
 from motor.motor_asyncio import *
 from config import *
 from domainers import *
+
 client = AsyncIOMotorClient(DATABASE_URL)
 db = client[DATABASE_NAME]
 col = db["users"]
 
-
-async def get_user(user_id):
-    user_id = int(user_id)
+async def get_user(user_id: int) -> dict:
     user = await col.find_one({"user_id": user_id})
     if not user:
         res = {
             "user_id": user_id,
-            "method":"MdiskPro",
+            "method": "Adlinkfly-Shortener-Bot",
             "shortener_api": None,
-            "mdisk_api": None,
-            "header_text": "",
-            "footer_text": "",
-            "username": None,
-            "base_site": "GreyMatterslinks.in",
-            "banner_image": None,
-            "is_banner_image": True,
-            "is_username": True,
-            "is_header_text": True,
-            "is_footer_text": True,
-            "include_domain": [],
-            "exclude_domain": [],
+            "base_site": BASE_SITE,  # Use the imported BASE_SITE value
             "banned": False
         }
         await col.insert_one(res)
         user = await col.find_one({"user_id": user_id})
-
     return user
 
 async def update_user_info(user_id, value:dict, tag="$set"):
@@ -58,15 +45,7 @@ async def is_user_exist(id):
     user = await col.find_one({'user_id':int(id)})
     return bool(user)
 """
-   _____                    __  __         _    _              _       _______           _     
-  / ____|                  |  \/  |       | |  | |            ( )     |__   __|         | |    
- | |  __  _ __  ___  _   _ | \  / |  __ _ | |_ | |_  ___  _ __|/ ___     | |  ___   ___ | |__  
- | | |_ || '__|/ _ \| | | || |\/| | / _` || __|| __|/ _ \| '__| / __|    | | / _ \ / __|| '_ \ 
- | |__| || |  |  __/| |_| || |  | || (_| || |_ | |_|  __/| |    \__ \    | ||  __/| (__ | | | |
-  \_____||_|   \___| \__, ||_|  |_| \__,_| \__| \__|\___||_|    |___/    |_| \___| \___||_| |_|
-                      __/ |                                                                    
-                     |___/                                                                     
-Author: GreyMatter's Tech
-GitHub: https://GreyMattersTech.com/GitHub
-Website: https://GreyMattersTech.com
+Author: StupidBoi
+Telegram: https://t.me/StupidBoi69
 """
+            
