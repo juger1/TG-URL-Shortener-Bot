3d69c778118f87a355636b76e0035bdf5753cb32
juger1 <gajehaj8@gmail.com>
2024-09-23 18:44:33 +0530

    Update contact.py

--- plugins/contact.py
+++ plugins/contact.py
@@ -8,12 +8,16 @@ async def contactAdmin(bot, message):
     try:
         user = "@" + message.from_user.username if message.from_user.username else message.from_user.mention
         if not message.reply_to_message:
-            return await message.reply("Please use the method described in the image to contact admin[.](https://telegra.ph/file/9a4039a2d602486cf1c00.jpg)")
+            return await message.reply("<blockquote><b>Please use the method described in the image to contact admin[.](https://telegra.ph/file/9a4039a2d602486cf1c00.jpg)</b></blockquote>")
         if not message.reply_to_message.text:
-            return await message.reply("Please use the method described in the image to contact admin[.](https://telegra.ph/file/9a4039a2d602486cf1c00.jpg)")
+            return await message.reply("<blockquote><b>Please use the method described in the image to contact admin[.](https://telegra.ph/file/9a4039a2d602486cf1c00.jpg)</b></blockquote>")
         await bot.send_message(
             chat_id=OWNER_ID, 
-            text=f"<bold>From:</bold> {user} <bold>Id:</bold> <code>{message.chat.id}</code>\n{message.reply_to_message.text.html}"
+            text=f"""<b>
+From: {user}
+Id: {message.chat.id}
+
+{message.reply_to_message.text.html}</b>"""
         )
         userMsg = await bot.send_message(
             chat_id=message.chat.id,
@@ -23,7 +27,7 @@ async def contactAdmin(bot, message):
         await asyncio.sleep(5)
         await userMsg.delete()
     except Exception as e:
-        return await message.reply(f"**Traceback Info:**\n`{traceback.format_exc()}`\n**Error Text:**\n`{e}`")
+        return await message.reply(f"<blockquote><b>Traceback Info:\n{traceback.format_exc()}\nError Text:\n{e}</b></blockquote>")
 
 @app.on_message(filters.private & filters.user(OWNER_ID))
 async def replyUser(bot, message):
@@ -47,18 +51,9 @@ async def replyUser(bot, message):
                 )
     except Exception as e:
         return await message.reply(
-            text=f"**Traceback Info:**\n`{traceback.format_exc()}`\n**Error Text:**\n`{e}`"
+            text=f"<blockquote><b>Traceback Info:\n{traceback.format_exc()}\nError Text:\n{e}</b></blockquote>"
         )
 """
-   _____                    __  __         _    _              _       _______           _     
-  / ____|                  |  \/  |       | |  | |            ( )     |__   __|         | |    
- | |  __  _ __  ___  _   _ | \  / |  __ _ | |_ | |_  ___  _ __|/ ___     | |  ___   ___ | |__  
- | | |_ || '__|/ _ \| | | || |\/| | / _` || __|| __|/ _ \| '__| / __|    | | / _ \ / __|| '_ \ 
- | |__| || |  |  __/| |_| || |  | || (_| || |_ | |_|  __/| |    \__ \    | ||  __/| (__ | | | |
-  \_____||_|   \___| \__, ||_|  |_| \__,_| \__| \__|\___||_|    |___/    |_| \___| \___||_| |_|
-                      __/ |                                                                    
-                     |___/                                                                     
-Author: GreyMatter's Tech
-GitHub: https://GreyMattersTech.com/GitHub
-Website: https://GreyMattersTech.com
+Author: StupidBoi
+Telegram: https://t.me/StupidBoi69
 """
